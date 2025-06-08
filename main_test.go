75eeee5a8532903b26dbf10d62be42e37ac41286
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2025-06-08 10:24:48 +0200

    refactor tests

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"testing"
 )
@@ -12,31 +13,7 @@ func TestParseCsv(t *testing.T) {
 	expected = append(expected, []string{"foo", "42", "23.12", "4.33"})
 	expected = append(expected, []string{"bar", "23", "2", "3"})
 
-	csvInputString, err := os.ReadFile("sample.csv")
-	if err != nil {
-		t.Fail()
-	}
-
-	csvOptionsString, err := os.ReadFile("sample.csv.options.json")
-	var csvOptions CsvOptions
-	if err != nil {
-		t.Fail()
-	}
-
-	check(json.Unmarshal(csvOptionsString, &csvOptions))
-
-	actual, err := parseCsv(csvInputString, csvOptions)
-	if err != nil {
-		t.Fail()
-	}
-
-	for rowIndex, rowValue := range actual {
-		for columnIndex, cellValue := range rowValue {
-			if cellValue != expected[rowIndex][columnIndex] {
-				t.Errorf("Expected %s to equal %s", cellValue, expected[rowIndex][columnIndex])
-			}
-		}
-	}
+	runTestParseCsv(t, "sample", expected)
 }
 
 func TestParseCsvSemicolon(t *testing.T) {
@@ -45,12 +22,16 @@ func TestParseCsvSemicolon(t *testing.T) {
 	expected = append(expected, []string{"foo", "42", "23.12", "4.33"})
 	expected = append(expected, []string{"bar", "23", "2", "3"})
 
-	csvInputString, err := os.ReadFile("sample-semicolon.csv")
+	runTestParseCsv(t, "sample-semicolon", expected)
+}
+
+func runTestParseCsv(t *testing.T, testCase string, expected [][]string) {
+	csvInputString, err := os.ReadFile(fmt.Sprintf("test-files/%s.csv", testCase))
 	if err != nil {
 		t.Fail()
 	}
 
-	csvOptionsString, err := os.ReadFile("sample-semicolon.csv.options.json")
+	csvOptionsString, err := os.ReadFile(fmt.Sprintf("test-files/%s.csv.options.json", testCase))
 	var csvOptions CsvOptions
 	if err != nil {
 		t.Fail()
